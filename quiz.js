df04e3e0de53d991961524b45e5bbee090de45be
MakeTheMaker <37077314+MakeTheMaker@users.noreply.github.com>
2021-12-30 22:47:24 +0200

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -82,7 +82,7 @@ var $indicators = $('<ol>')
 
   $("<button>")
     .attr('class', 'quiz-button btn')
-    .text("Take the quiz!")
+    .text("Aloita!")
     .click(function() {
       $quiz.carousel('next');
       $indicators.addClass('show');
